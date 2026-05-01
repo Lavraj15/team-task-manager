b5a628db53b02e6527e0b271d96963efc1d7e17d
Lav Raj <lavraj@bradtechnology.com>
2026-05-01 12:23:01 +0530

    deploye

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://team-task-manager-production-2955.up.railway.app"
+  baseURL: "https://team-task-manager-2-d7ov.onrender.com/api"
 });
 
 // 🔥 token auto send
